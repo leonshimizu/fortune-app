2af12f00c800f23df174022660ac0906a64d2c5c
Leon <lmshimizu@gmail.com>
2021-10-10 09:27:27 -0700

    added feature 1 - random fortune

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,16 @@
 class MyExamplesController < ApplicationController
+  def random_fortune # feature 1
+    number = Random.new
+    random_number = number.rand(3)
+
+    if random_number == 1
+      fortune = "You win"
+    elsif random_number == 2
+      fortune = "Nothing happens"
+    elsif random_number == 3
+      fortune = "You lose"
+    end
+
+    render json: {fortune: fortune}
+  end
 end
